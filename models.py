905dcd68f054764a713b067d06817ff2790e0483
Jan <102752130+k0nfus@users.noreply.github.com>
2023-04-03 18:26:00 +0200

    Add files via upload

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Meldung(models.Model):
-    betreff = models.CharField(null=False, max_length=250)
-    text = models.TextField(null=True, max_length=1000)
+    betreff = models.CharField(null=False, max_length=250, verbose_name="Meldegrund")
+    text = models.TextField(null=True, max_length=1000, verbose_name="Meldung")
     datum = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name_plural = "Meldungen"
+    
     def __str__(self):
         return self.betreff
